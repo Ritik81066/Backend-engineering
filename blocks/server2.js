e885b4c6ff80b1b7728b6e4d124dffb70299c3e0
Ritik Goyal <ritikgoyal4346@gmail.com>
2025-03-31 17:06:23 +0530

    initial commit

--- blocks/server2.js
+++ blocks/server2.js
@@ -1,59 +1,94 @@
-const express = require('express');
-const mongoose = require('mongoose');
+//ODM-> Object Document Mapper
+const express=require("express");
+const app=express()
+const mongoose=require("mongoose");
+const User=require("./model/userModel")
+const Blog=require("./model/blogModel")
 
-const user = require('./model/user');
-
-const app = express();
-
-const PORT = 5000;
-
-app.post('/users',async(req,res)=>{
-    let {name,email,password} = req.body;
-    let newUser = new user({
-        name:name,
-        email:email,
-        password:password
-    });
-    await newUser.save();
-    res.send("User added");
+app.post("/users",async(req,res)=>{
+  let {name,email,password}=req.body;
+  let newUser=new User({
+    name:name,
+    email:email,
+    password:password,
+  });
+  await newUser.save()
+  res.send("User Added")
 })
 
-app.get('/users',async(req,res)=>{
-    let allusers = await user.find();
-    res.send(allusers);
+//read all users
+app.get("/users",async(req,res)=>{
+  let allUsers=await User.find();
+  res.send(allUsers);
 })
 
-app.get('/',(req,res)=>{
-    res.status(200).send("Hello Welcome to Home Page");
+//read one user
+app.get("/users/:id",async(req,res)=>{
+  let {id}=req.params;
+  let oneUser=await User.findById(id);
+  res.send(oneUser)
+})
+//delete user
+app.delete("/user/id",async(req,res)=>{
+  let {id}=req.params;
+  await User.findByIdAndDelete()
+  res.send("User deleted")
+})
+//update a user
+app.put("users/:id",async(req,res)=>{
+  let {id}=req.params;
+  let {name,email,password}=req.body;
+  let updateUser=await User.findById(id);
+  updateUser.name=name;
+  updateUser.email=email;
+  updateUser.password=password;
+  await updateUser.save();
 })
 
-app.delete('/user/:id', async(req,resp)=>{
-    let {id}=req.params;
-    await user.findByIdAndDelete(id);
-    resp.send("user deleted");
-})
-app.put("/users/:id",async(req,res)=>{
-    let {id} =req.params;
-    let {name,email,password}=req.body;
-    let updateUser=await UserActivation.findById(id);
-   // {
-        //name:"Nitesh",
-        //email:"bubh@yftc.com",
-        //password:"qwerty"
-    // }
-    updateUser.name=name;
-    updateUser.password=password;
-    updateUser.email=email;
-    await updateUser.save();
-    res.send("User Updated");
-})
-mongoose.connect('mongodb://localhost:27017/Mongoose-1')
-.then(()=>console.log("Connected!!"))
-.catch((err) => console.error("Connection error:", err));
 
-app.listen(PORT,()=>{
-    console.log(`server listening at http://localhost:${PORT}`);
+//add one blog
+app.post("/blogs",async(req,res)=>{
+  let {title,content,author,date}=req.body;
+  let newBlog=new Blog({
+    title:title,
+    content:content,
+    author:author,
+    date:date
+  });
+  await newBlog.save()
+  res.send("Blog Added")
 })
 
-/*
-*/
+//read all blogs
+app.get("/blogs",async(req,res)=>{
+  let allBlogs=await Blog.find();
+  res.send(allBlogs);
+})
+
+//read one blog
+app.get("/blogs/:id",async(req,res)=>{
+  let {id}=req.params;
+  let oneBlog=await Blog.findById(id);
+  res.send(oneBlog)
+})
+
+app.put("blogs/:id",async(req,res)=>{
+  let {id}=req.params;
+  let {title,content,author,date}=req.body;
+  let updateBlog=await Blog.findById(id);
+  updateBlog.title=title;
+  updateBlog.content=content;
+  updateBlog.author=author;
+  updateBlog.date=date
+  await updateBlog.save();
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/mongooseDB')
+  .then(() => console.log('Connected!'));
+app.listen(2000,()=>{
+    console.log("Server started");
+})
+
+
+
+
